9412f506571db53d567f41a422f79fb6a20ae227
nia-hamilton <nia.hamilton@stageslearning.com>
2022-05-23 17:21:30 -0400

    Reformatted Functions.php

--- front-page.php
+++ front-page.php
@@ -1,10 +1,32 @@
 <?php get_header();?>
 
-<div class="fill">
- oyoyo
+<div class="container">
 
+  <h1>Hi, my name is Nia!</h1>
+  <h2>In everything I do, I want to promote understanding and communication.</h2>
+
+  <pre>
+    <code>
+
+    .some-class{
+      ohmigosh
+    }
+
+    </code>
+  </pre>
 
 </div>
+
+<p class="scroll-text">what I do:</p>
+
+<div class="roles">
+  <button class="button-primary">UI/UX Design</button>
+  <button class="button-primary">Frontend Web Design</button>
+  <button class="button-primary">Project Management</button>
+  <button class="button-primary">Learning/Instructional Design</button>
+  <button class="button-primary">Language Tutoring</button>
+</div>
+
 
 
 <?php get_footer();?>
--- functions.php
+++ functions.php
@@ -1,19 +1,18 @@
  <?php
 
+//Stylesheet Function
 function load_stylesheets(){
 
   //Skeleton CDN
-  wp_enqueue_style( 'skeleton', 'https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css' );
-
-  // Regular Stylesheet
+  wp_enqueue_style( 'skeleton', 'https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css' );
+  // style.css
   wp_register_style('stylesheet', get_template_directory_uri() . '/style.css', array(), false, 'all');
   wp_enqueue_style('stylesheet');
 
 }
-
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
-
+// Javascript Function
 function loadjs(){
 
   wp_register_script('customjs', get_template_directory_uri() . '/scripts.js', '', 1, true);
